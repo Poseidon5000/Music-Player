a119a405442e414f794bcbcc72af0543e154d92e
Johnson <jhnsnakpan@gmail.com>
2023-03-09 12:28:55 -0800

    update

--- music.js
+++ music.js
@@ -1,24 +1,25 @@
-const img = document.querySelector("#img")
-const artist = document.querySelector("#artist")
-const song = document.querySelector("#song")
-const btn = document.querySelector("#btn")
+// const img = document.querySelector("#img")
+// const artist = document.querySelector("#artist")
+// const song = document.querySelector("#song")
+// const btn = document.querySelector("#btn")
 
-let searchBar = document.querySelector("#input").value
+// let searchBar = document.querySelector("#input").value
 
 
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "8b567cd1f4mshe3add9253ac36e2p1f2a47jsn668f76c60d98",
-    "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
-  },
-};
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "8b567cd1f4mshe3add9253ac36e2p1f2a47jsn668f76c60d98",
+//     "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+//   },
+// };
 
-fetch(
-  "https://spotify23.p.rapidapi.com/artists/?ids=2w9zwq3AktTeYYMuhMjju8",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+// fetch(
+//   "https://spotify23.p.rapidapi.com/artists/?ids=2w9zwq3AktTeYYMuhMjju8",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
